a55a1449e90d0160a8e76e6eae90073db808cbc9
Luca Viana <78035167+O-orion@users.noreply.github.com>
2022-04-21 17:46:27 -0400

    Primeiro projeto react native

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>JESUS CRISTO É O SENHOR !!</Text>
+      <Text> Lucas Viana 17:43 </Text>
       <StatusBar style="auto" />
     </View>
   );
